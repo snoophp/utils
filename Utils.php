7b89c348d04259f1f66a77dd4e2058319e095548
Sneppy <andrea.mecchia@mail.polimi.it>
2018-05-22 16:15:52 +0200

    Moved Utils and helpers in Utils module + helpers in Vue module

---
+++ Utils.php
@@ -0,0 +1,270 @@
+<?php
+
+namespace SnooPHP;
+
+use SnooPHP\Http\Request;
+use SnooPHP\Vue\Component;
+
+/**
+ * Set of utility methods
+ * 
+ * @author sneppy
+ */
+class Utils
+{
+	/**
+	 * Include view
+	 * 
+	 * @param string	$name		view name
+	 * @param array		$args		arguments to expose
+	 * @param Request	$request	request if differs from current request
+	 * 
+	 * @return bool
+	 */
+	public static function view($name, array $args, Request $request = null)
+	{
+		$request = $request ?: Request::current();
+		$fullPath = path("views")."/{$name}.php";
+		if (file_exists($fullPath))
+		{
+			include $fullPath;
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Include a vue component
+	 * 
+	 * @param string	$file		component file path
+	 * @param array		$args		arguments to expose
+	 * @param Request	$request	request if differs from current request
+	 * 
+	 * @return bool
+	 */
+	public static function vueComponent($file, array $args, Request $request = null)
+	{
+		$request	= $request ?: Request::current();
+		$file		= substr($file, 0, 2) === "@/" ? path("views/components/".substr($file, 2).".vue.php") : $file;
+		$component	= new Component($file, $args, $request);
+		if ($component->valid())
+		{
+			// Register component
+			$GLOBALS["vue"]->register($component);
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * Merge together styles and scripts blocks after body
+	 * 
+	 * @ignore don't use this function
+	 * 
+	 * @param string $content a valid html document content
+	 * 
+	 * @return string
+	 */
+	public static function optimizeView($content)
+	{
+		// Valid html only contains one body block
+		$bodyStart	= strpos($content, "<body");
+		$bodyEnd	= strpos($content, "</body") + 7;
+		if ($bodyStart !== false && $bodyEnd !== false)
+		{
+			$preBody	= substr($content, 0, $bodyStart);	
+			$body		= substr($content, $bodyStart, $bodyEnd - $bodyStart);	
+			$postBody	= substr($content, $bodyEnd);
+			$styles		= "";
+			$scripts	= "";
+			
+			/** @todo not sure if it's a good idea */
+			if (false)
+			{
+				// Match all style and script blocks
+				// Content is merged together and passed to the css preprocessor (if any)
+				if (preg_match_all("~(?:<style>\n?((?:(?!</).)+)</style>|<script>\n?((?:(?!</script>).)+)</script>)~s", $postBody, $matches))
+				{
+					foreach ($matches[1] as $styleContent)	$styles		.= $styleContent;
+					foreach ($matches[2] as $scriptContent)	$scripts	.= $scriptContent;
+				}
+				$postBody = "<script>$scripts</script>\n<style>$styles</style>";
+
+				// Recompose
+				$content = $preBody.$body.$postBody;
+			}
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Compile style content using specified css preprocessor
+	 * 
+	 * @param string	$content	content to compile
+	 * @param string	$lang		css preprocessor (default to vanilla css, no process)
+	 * 
+	 * @return string compiled content
+	 */
+	public static function compileStyle($content, $lang = "vanilla")
+	{
+		$compiled	= $content;
+		$content	= preg_replace("/\"/", "\\\"", $content);
+		switch ($lang) {
+			case "lessc":
+			case "less":
+				if (empty(`which lessc`))
+				{
+					error_log("lessc module not found (run `npm i -g lessc` to install)");
+					break;
+				}
+
+				$compiled = `echo "$content" | lessc - --compress`;
+				break;
+			
+			case "stylus":
+				if (empty(`which stylus`))
+				{
+					error_log("stylus module not found (run `npm i -g stylus` to install)");
+					break;
+				}
+
+				$compiled = `echo "$content" | stylus --compress`;
+				break;
+			
+			default:
+				/* Nothing to compile */
+				break;
+		}
+
+		// Return result
+		return $compiled;
+	}
+
+	/**
+	 * Minify javascript using uglify-js if available
+	 * 
+	 * @param string $content content to minify
+	 */
+	public static function minifyJs($content)
+	{
+		if (!empty(`which uglifyjs`))
+		{
+			return `echo "$content" | uglifyjs --compress --mangle --mangle-props`;
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Convert to json string
+	 * 
+	 * @deprecated 1.0.1
+	 * 
+	 * @param mixed $content content to convert
+	 * 
+	 * @return string
+	 */
+	public static function toJson($content)
+	{
+		return to_json($content);
+	}
+
+	/**
+	 * Decode from json string
+	 * 
+	 * @deprecated 1.0.1
+	 * 
+	 * @param string	$content	content to decode
+	 * @param bool		$assoc		if true return array rather than object
+	 * 
+	 * @return object|array
+	 */
+	public static function fromJson($content, $assoc = false)
+	{
+		return from_json($content, $assoc);
+	}
+
+	/**
+	 * Get or set session errors
+	 * 
+	 * @param string $err error string
+	 */
+	public static function errors($err = null)
+	{
+		// Start session
+		if (!isset($_SESSION)) session_start();
+		if ($err)$_SESSION["errors"][] = $err;
+		return isset($_SESSION["errors"]) ? $_SESSION["errors"] : [];
+	}
+
+	/**
+	 * Flush all errors
+	 */
+	public static function flushErrors()
+	{
+		if (isset($_SESSION)) unset($_SESSION["errors"]);
+	}
+
+	/**
+	 * Get path relative to public folder
+	 * 
+	 * @param string $absolutePath absolute path
+	 * 
+	 * @return string|bool
+	 */
+	public static function publicPath($absolutePath)
+	{
+		$publicPath = path("public");
+		if (preg_match("@^{$publicPath}(.*)$@", $absolutePath, $matches))
+		{
+			return $matches[1];
+		}
+
+		return false;
+	}
+
+	/**
+	 * Parse string as value
+	 * 
+	 * @param string $val string to parse
+	 * 
+	 * @return mixed
+	 */
+	public static function parseValue($val)
+	{
+		$val = trim($val);
+		if (preg_match("/^(?:TRUE|FALSE|ON|OFF)$/i", $val))				return strcasecmp($val, "TRUE") === 0 || strcasecmp($val, "ON") === 0;
+		else if (preg_match("/^[0-9]+$/", $val) && strlen($val) < 16)	return (int)$val;
+		else if (preg_match("/^[0-9]*\.(?:[0-9]+f?|f)$/", $val))		return (float)$val;
+
+		return $val;
+	}
+
+	/**
+	 * Check ip address against a test ip
+	 * 
+	 * Ips should be in x.y.z.x/w form, where w is the mask
+	 * 
+	 * @param string	$ip		ip address to check
+	 * @param string	$test	ip address used as test
+	 * 
+	 * @return bool
+	 */
+	public static function validateIp($ip, $test)
+	{
+		if (!is_string($ip) || !is_string($test)) return false;
+		$testBytes = preg_split("@(?:\.|/)@", $test);
+		$ipBytes = preg_split("@(?:\.|/)@", $ip);
+		$test = unpack("N", pack("C*", $testBytes[0], $testBytes[1], $testBytes[2], $testBytes[3]))[1];
+		$ip = unpack("N", pack("C*", $ipBytes[0], $ipBytes[1], $ipBytes[2], $ipBytes[3]))[1];
+		if (isset($testBytes[4]) && $mask = (int)$testBytes[4])
+		{
+			$test = $test >> (32 - $mask);
+			$ip = $ip >> (32 - $mask);
+		}
+		return $test === $ip;
+	}
+}
---
+++ helpers.php
@@ -0,0 +1,192 @@
+<?php
+
+if (!function_exists("from_json"))
+{
+	/**
+	 * Decode from json
+	 * 
+	 * @param string	$content	json string
+	 * @param bool		$assoc		if true return an array rather than an object
+	 * 
+	 * @return array|object
+	 */
+	function from_json($content, $assoc = false)
+	{
+		return json_decode($content, $assoc);
+	}
+}
+
+if (!function_exists("mime_type"))
+{
+	/**
+	 * Alias for deprecated mime_content_type
+	 * 
+	 * uses Fileinfo
+	 * 
+	 * @param string $filename file to test
+	 * 
+	 * @return string mime type
+	 */
+	function mime_type($filename)
+	{
+		$fileInfo	= new finfo();
+		$type		= $fileInfo ? $fileInfo->file($filename, FILEINFO_MIME_TYPE) : false;
+		error_log($type);
+		// If plain/text, try to use extension
+		if ($type === "text/plain")
+		{
+			$ext = pathinfo($filename, PATHINFO_EXTENSION);
+			switch ($ext)
+			{
+				case "css":
+					return "text/css";
+				case "js":
+					return "text/javascript";	
+				default:
+					return "text/plain";
+			}
+		}
+
+		return $type;
+	}
+}
+
+if (!function_exists("path"))
+{
+	/**
+	 * Return absolute path for project directory
+	 * 
+	 * @param string	$path	relative path
+	 * @param bool		$safe	return false if file/directory doesn't exist
+	 * 
+	 * @return string|bool
+	 */
+	function path($path = "", $safe = false)
+	{
+		$rootDir = defined("ROOT_DIR") ? ROOT_DIR : $_SERVER["DOCUMENT_ROOT"];
+		return $safe ?
+		realpath($rootDir."/".$path) :
+		$rootDir."/".$path;
+	}
+}
+
+if (!function_exists("path_relative"))
+{
+	/**
+	 * Return relative path (relative to project root folder by default)
+	 * 
+	 * @param string		$path	absolute path
+	 * @param string|null	$root	the root folder by default
+	 * 
+	 * @return string|bool
+	 */
+	function path_relative($path, $root = null)
+	{
+		$root = $root ?: path();
+		if (preg_match("@^$root@", $path))
+			return substr($path, strlen($root));
+		else
+			return false;
+	}
+}
+
+if (!function_exists("read_file"))
+{
+	/**
+	 * Return content from file
+	 * 
+	 * It is really just an alias for @see file_get_contents()
+	 * 
+	 * @param string $path path to file
+	 * 
+	 * @return string|bool false if fails
+	 */
+	function read_file($path)
+	{
+		// Calc real path
+		$path = ltrim($path);
+		if ($path[0] === '/')	$path = rtrim($path);
+		else					$path = path($path);
+
+		return file_get_contents($path);
+	}
+}
+
+if (!function_exists("to_json"))
+{
+	/**
+	 * Encode content as json string
+	 * 
+	 * @param string|array|object $content content to encode
+	 * 
+	 * @return string
+	 */
+	function to_json($content)
+	{
+		return is_string($content) ?
+		$content :
+		json_encode($content);
+	}
+}
+
+if (!function_exists("to_utf8"))
+{
+	/**
+	 * Encode content as utf8 string
+	 * 
+	 * @param string $content content to encode
+	 * 
+	 * @return string
+	 */
+	function to_utf8($content)
+	{
+		return mb_convert_encoding($content, "UTF-8");
+	}
+}
+
+if (!function_exists("view"))
+{
+	/**
+	 * Include view
+	 * 
+	 * @see SnooPHP\Utils::view()
+	 */
+	function view($name, array $args = [], SnooPHP\Http\Request $request = null)
+	{
+		SnooPHP\Utils::view($name, $args, $request);
+	}
+}
+
+if (!function_exists("write_file"))
+{
+	/**
+	 * Put content to file using put_content native php function
+	 * 
+	 * @param string	$path		path to file
+	 * @param mixed		$content	string, binary or object/array content (converted to json)
+	 * @param bool		$createDir	if directories don't exist create them
+	 * @param bool		$serialize	if true, objects and arrays will be serialized rather than converted to json
+	 * 
+	 * @return bool false if fails
+	 */
+	function write_file($path, $content, $createDir = true, $serialize = false)
+	{
+		// Calc real path
+		$path = ltrim($path);
+		if ($path[0] === '/')	$path = rtrim($path);
+		else					$path = path($path);
+
+		// Check if dir exists
+		// Create it otherwise
+		$dir = dirname($path);
+		if (!file_exists($dir) && (!$createDir || !mkdir($dir, 0755, true))) return false;
+
+		// Convert content
+		$content = is_string($content) ? $content : (
+			$serialize ? serialize($content) : to_json($content)
+		);
+
+		// Write file
+		return file_put_contents($path, $content) !== false;
+	}
+}
